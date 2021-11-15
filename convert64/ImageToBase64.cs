992e1dff45c03271bfda8ffd0e55a0dff7870606
Marzeu <marzahell@hotmail.com>
2021-11-14 23:38:23 -0300

    finalizado

---
+++ convert64/ImageToBase64.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+
+namespace convert64
+{
+    class ImageToBase64
+    {
+        string path = System.AppDomain.CurrentDomain.BaseDirectory.ToString();
+
+        public string ConverterParaBase64(string path)    
+        {
+
+            string base64String = Convert.ToBase64String(File.ReadAllBytes(path));
+
+            return "data:image/gif;base64," + base64String;
+        }
+    }
+}
--- convert64/Program.cs
+++ convert64/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,19 @@ namespace convert64
     {
         static void Main(string[] args)
         {
+            string caminhoApp = System.AppDomain.CurrentDomain.BaseDirectory.ToString();
+            string caminhoGif = caminhoApp + @"Resorce\homer.gif"; 
+
+            string image64 = ConverterParaBase64(caminhoGif);
+            Console.WriteLine(image64);
+        }
+
+        public static string ConverterParaBase64(string path)
+        {
+
+            string base64String = Convert.ToBase64String(File.ReadAllBytes(path));
+
+            return "data:image/gif;base64," + base64String;
         }
     }
 }
